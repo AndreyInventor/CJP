562c5c15b8c9bd29e1c1416a09766985d3fdfa39
Andrey <inventor@cn.ru>
2020-09-17 21:56:10 +0700

    Add project files.

---
+++ CJP.py
@@ -0,0 +1,8 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+#from auth_data import username, password
+import time
+import random
+from selenium.common.exceptions import NoSuchElementException
+
+
